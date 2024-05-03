6021f16bcc97c3c113199b0208a8876d3e7aa495
Daniel Gomez <dgomez@linq.com>
2024-05-02 21:58:31 -0500

    Add Time zones to recommendations

--- src/script.js
+++ src/script.js
@@ -6,6 +6,10 @@ fetch('../resources/travel_recommendation_api.json')
   .then((response) => response.json())
   .then((json) => (data = json));
 
+searchInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') search();
+});
+
 function resetSearch() {
   searchInput.value = '';
   recommendations.innerHTML = '';
@@ -22,6 +26,7 @@ function search() {
 function showRecommendations(places) {
   recommendations.innerHTML = '';
   places.forEach((place) => {
+    recommendations.innerHTML += getCountryTimeCard(place);
     const card = `
     <div class="recommendation-card">
     <img src="resources/${place.imageUrl}" alt="">
@@ -50,4 +55,34 @@ function searchByCity(userEntry) {
     if (country.name.toLowerCase().includes(userEntry)) return country.cities;
   }
   return [];
+}
+
+function getCountryTimeCard(place) {
+  const country = place.name.split(', ')[1];
+  const city = place.name.split(', ')[0];
+  const timeZone = getTimeZone(country);
+  return timeZone
+    ? `<div class="localTime-card">
+        Current time in ${country} - ${city}: <span>${timeZone}</span>
+      </div>`
+    : '';
+}
+
+function getTimeZone(country) {
+  const timezones = {
+    Australia: `Australia/Sydney`,
+    Brazil: 'America/Sao_Paulo',
+    Japan: 'Asia/Tokyo',
+    India: 'Asia/Kolkata',
+  };
+  if (timezones[country]) {
+    const options = {
+      timeZone: timezones[country],
+      hour12: true,
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    };
+    return new Date().toLocaleTimeString('en-US', options);
+  }
 }
